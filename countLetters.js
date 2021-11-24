0a01c843a390ea340faf29b1c1984b4df85d8e53
Fardosa Garad <fardosamgarad@gmail.com>
2021-11-24 15:58:36 -0500

    implemented count letters and count only

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+ 
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
+
+const countLetters =  function(str) {
+  let characterArray = []
+  for (const value of str) {
+    if (!characterArray.includes(value)) {
+      characterArray.push(value) 
+      console.log(value + " : " + countLetter(str,value))
+    } 
+  
+  }
+}
+const countLetter = function(str,char){
+  let counter = 0
+  for (const value of str) {
+    if (value === char) {
+      counter++
+    }
+  } 
+  return counter
+}
+countLetters("letters")
---
+++ without.js
@@ -0,0 +1,14 @@
+const eqArrays = function (arr1,arr2) {
+  for (i=0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+    return false
+  } return true
+}
+const assertArrayEqual = function (arr1,arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log("true")
+  } else {
+    console.log("false")
+  }
+}
+

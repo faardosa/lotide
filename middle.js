842c7ccfb0c17317e029c10fff74257bce136e4d
Fardosa Garad <fardosamgarad@gmail.com>
2021-11-24 16:27:31 -0500

    middle and letter

---
+++ middle.js
@@ -0,0 +1,31 @@
+const eqArrays = function (arr1,arr2) {
+  for (i=0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+    return false
+  } return true
+}
+const assertArrayEqual = function (arr1,arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log("true")
+  } else {
+    console.log("false")
+  }
+}
+const eqArrays = function (arr1,arr2) {
+  for (i=0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+    return false
+  } return true
+}
+const middle = function(array) {}
+  let result = []; 
+  let middle = Math.floor(array.length / 2)
+
+  console.log("middle", middle)
+  if (array.length % 2){
+      result.push(array[middle])
+    
+  } 
+  else {
+    result.push(array[middle - 1])
+    result.push(array[middle]) }

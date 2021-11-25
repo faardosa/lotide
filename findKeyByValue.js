3bf2b6e6846f7d993a86a9371ac063043f9ea730
Fardosa Garad <fardosamgarad@gmail.com>
2021-11-24 20:03:48 -0500

    find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(obj,val) {
+  for (const element in obj) {
+    if (obj[element] === val) {
+      return element
+    }
+  } 
+};
+
+
+//console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"))
+
+
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

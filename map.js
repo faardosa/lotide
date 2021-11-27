0450ae9b01c8746136a373ff7bff833dfc9c7dff
Fardosa Garad <fardosamgarad@gmail.com>
2021-11-26 21:52:40 -0500

    map lotide function

---
+++ map.js
@@ -0,0 +1,23 @@
+const eqArrays = function (arr1,arr2) {
+  for (i=0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+    return false
+  } return true
+}
+const assertArrayEqual = function (arr1,arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log("true")
+  } else {
+    console.log("false")
+  }
+}
+
+const map = function(array, callback) {
+
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+
+};

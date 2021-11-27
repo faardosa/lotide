323cd5653f6f3c2d32b0399b5d1ce4e4b10140ea
Fardosa Garad <fardosamgarad@gmail.com>
2021-11-26 21:47:17 -0500

    more lotide functions

---
+++ findKey.js
@@ -0,0 +1,6 @@
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) return key;
+  }
+};
+
---
+++ takeUntil.js
@@ -0,0 +1,12 @@
+const takeUntil = function(array, callback) {
+  let answer = [];
+  for (let element of array) {
+    if (callback(element)) {
+      break;
+    } else {
+      answer.push(element);
+    }
+  }
+  return answer;
+};
+
